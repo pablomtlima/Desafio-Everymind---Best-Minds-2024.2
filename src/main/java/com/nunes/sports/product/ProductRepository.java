a5430262d00018f7133f4e5f34beb9a6103f2a3b
Pablo Matos Lima <78696750+pablomtlima@users.noreply.github.com>
2024-08-26 15:20:44 -0300

    Delete src directory

--- src/main/java/com/nunes/sports/product/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.nunes.sports.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- src/main/java/com/nunes/sports/product/ProductResponse.java
+++
@@ -1,7 +0,0 @@
-package com.nunes.sports.product;
-
-public record ProductResponse(Long id, String name, String description, double price) {
-    public ProductResponse(Product product){
-        this(product.getId(), product.getName(), product.getDescription(), product.getPrice());
-    }
-}
